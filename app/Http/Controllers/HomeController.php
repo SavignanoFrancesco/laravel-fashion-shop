e1bd4232c8980321f6cfafdd9decc4536a463e79
Francesco <francesco.savignano96@gmail.com>
2021-01-19 15:40:35 +0100

    aggiunta route alla view 'products' al HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,4 +7,21 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
+    public function index(){
+        //collections da passare alla view
+        $data = [
+
+        ];
+        //pagina da mostrare
+        return view('home');
+    }
+    //
+    public function products(){
+        //collections da passare alla view
+        $data = [
+
+        ];
+        //pagina da mostrare
+        return view('products');
+    }
 }
